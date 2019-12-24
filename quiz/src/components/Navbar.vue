c9f29dc5f701bb1996512c80711a31f5f03eedc5
Jan <jan.leusmann@gmx.de>
2019-12-24 17:34:48 +0100

    intial page & grid system

---
+++ quiz/src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <v-navigation-drawer v-model="drawer" app>
+    <v-list>
+      <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+        <v-list-item-icon>
+          <v-icon class="black--text">{{ link.icon }}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title class="black--text">{{ link.text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer> 
+
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-toolbar-title app>
+      <span class="font-weight-light">Antim</span>
+        <span>8</span>
+        <span class="font-weight-light">s</span>
+    </v-toolbar-title>
+  </v-app-bar> 
+  </nav>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      drawer: false,
+      links: [
+        { icon: 'mdi-view-dashboard', text: 'Home', route: '/' },
+        { icon: 'mdi-folder', text: 'About', route: '/about' },
+      ]
+      }
+    },
+  }
+</script>
---
+++ quiz/src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container class="grey lighten-5">
+    <template v-for="m in 4">
+    <v-row :key="m">
+      <template v-for="q in Questions">
+        <v-col :key="q.nr">
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            {{q.text}}
+          </v-card>
+        </v-col>
+      </template>
+    </v-row>
+    </template>
+  </v-container>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+      Questions: [
+        { text: 'Q1', nr: 1},
+        { text: 'Q2', nr: 2 },
+      ]
+      }
+    },
+  }
+</script>
